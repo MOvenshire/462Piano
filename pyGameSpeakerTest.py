3d5d101fa6a96e9ecc71e3a74dffbc4ec5fee9c3
Marissa Ovenshire <movenshire@tamu.edu>
2023-11-01 15:29:37 -0500

    add wav and code files for speaker test

---
+++ pyGameSpeakerTest.py
@@ -0,0 +1,15 @@
+import pygame
+#file setup
+path = "/home/pi/Documents/piano_note_wav/"
+sound_files = ["piano-e_E_major.wav", "piano-d_D_major.wav","piano-c_C_major.wav","piano-b_B_major.wav","piano-a_A_major.wav"]
+
+#pygame setup
+pygame.mixer.init()
+speaker_volume = 0.5 #50% vol
+pygame.mixer.music.set_volume(speaker_volume)
+
+for f in sound_files:
+    pygame.mixer.music.load(path+f)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        continue
