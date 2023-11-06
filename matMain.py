81d1d31fc7d8a961697207fd4b7747edd0d1bfd3
MOvenshire <112366119+MOvenshire@users.noreply.github.com>
2023-11-06 13:07:44 -0600

    Add comment with notes and numbers to matMain.py

--- matMain.py
+++ matMain.py
@@ -3,8 +3,9 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
-buttonArr =[14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21, 17, 27, 22, 5, 6, 13, 19]
+# Buttons C[middle], D, E, F, G, A, B, C, D, E, F, C#, D#, F#, G#, A#, C#, D#
+# Buttons   1   2   3   4   5   6   7  8  9   11  12  13  14  15  16  17 18
+buttonArr =[14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21, 17, 27, 22, 5, 6]
 for b in buttonArr:
   GPIO.setup(b, GPIO.IN, GPIO.PUD_UP)
 
